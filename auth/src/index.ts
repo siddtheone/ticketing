cd520a7f3512adbef0cb13b4e685ba5a3fef8d68
siddtheone <45398971+siddtheone@users.noreply.github.com>
2022-05-30 23:00:41 +0530

    added startup message (#1)
    * added startup message
    
    * Test fail
    
    * reverting wrong code
    
    * adding order service
    
    * Adding payments and tickets CI
    
    * resting
    
    * Logging for orders
    
    * updating msg
    
    Co-authored-by: sdfg <werfsfdsf@iosdufoi.com>

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { app } from "./app";
 const PORT = 3000;
 
 const start = async () => {
+  console.log("Starting up auth service");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
